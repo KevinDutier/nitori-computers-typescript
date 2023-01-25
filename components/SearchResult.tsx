c3d95b9b24557fbad5b62a5e47548ed419b63920
KevinDutier desktop <kevin.dutier@gmail.com>
2023-01-25 21:27:02 +0100

    added "sort by" popover

--- components/SearchResult.tsx
+++ components/SearchResult.tsx
@@ -6,6 +6,11 @@ import { useEffect, useState } from "react";
 
 import { TailSpin } from "react-loader-spinner"; // spinner displayed when waiting for result
 
+import { Popover, Typography } from "@mui/material"; // used for the "sort by" popover
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSort } from "@fortawesome/free-solid-svg-icons";
+
 export const SearchResult: React.FC = () => {
   // what this function does:
   // 1st (on page load): fetches the backend, searches database with search input (router.query.parameter) (ex: search database for "laptop")
@@ -15,14 +20,16 @@ export const SearchResult: React.FC = () => {
 
   const [articles, setArticles] = useState([]);
   const [resultFound, setResultFound] = useState(undefined); // before search result comes, resultFound is undefined
-  const [productCards, setProductCards] = useState(<></>); // before the search result comes, product card is empty
+  const [productCards, setProductCards] = useState(<></>); // before search result comes, product card is empty
+  const [sortBy, setSortBy] = useState("byBrand"); // by default, articles are sorted by brand
+  const [anchorEl, setAnchorEl] = useState(null); // popover menu anchor
 
   // search function, that searches for article data in the database
   // called on page load
   const search = async () => {
     const res = await fetch(
       // searches for articles in database, using router.query.parameter (from the url)
-      `${process.env.NEXT_PUBLIC_BACKEND_ADDRESS}articles/search/${router.query.parameter}/byPrice`
+      `${process.env.NEXT_PUBLIC_BACKEND_ADDRESS}articles/search/${router.query.parameter}/${sortBy}`
     );
     const response = await res.json(); // converts response to json format
 
@@ -35,15 +42,86 @@ export const SearchResult: React.FC = () => {
     // if (!router.query.parameter) return // wait for router.query.parameter (url parameter) before proceeding
 
     if (router.query.parameter) {
-      setProductCards(<></>) // reset productCards every time user searches
       search(); // router.query.parameter (url parameter) has been obtained: proceed and execute database search
     }
-    
-  }, [router.query.parameter]);
+  }, [router.query.parameter, sortBy]);
+
+  // "SORT BY" POPOVER
+
+  // opens the popover
+  function openPopover(event: any) {
+    // sets the popover's anchor to the cart icon
+    setAnchorEl(event.currentTarget);
+  }
+
+  // closes the popover
+  function closePopover() {
+    // sets anchor to null
+    setAnchorEl(null);
+  }
+
+  const sortPopover = () => {
+    return (
+      <div className={styles.sortByContainer}>
+
+        <div className={styles.flexContainer}>
+          <p className={styles.sortBy}>sort by:</p>
+          <FontAwesomeIcon
+            className={styles.popoverIcon}
+            icon={faSort}
+            onClick={openPopover}
+          />
+          <Popover
+            open={Boolean(anchorEl)}
+            onClose={() => closePopover()}
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+          >
+            <Typography
+              className={styles.popoverItem}
+              onClick={() => {
+                setSortBy("byBrand");
+                closePopover();
+              }}
+            >
+              brand (A-Z)
+            </Typography>
+            <Typography
+              className={styles.popoverItem}
+              onClick={() => {
+                setSortBy("byCategory");
+                closePopover();
+              }}
+            >
+              category (A-Z)
+            </Typography>
+            <Typography
+              className={styles.popoverItemLast}
+              onClick={() => {
+                setSortBy("byPrice");
+                closePopover();
+              }}
+            >
+              price (low to high)
+            </Typography>
+          </Popover>
+        </div>
+      </div>
+    );
+  };
+
+  // END OF "SORT BY" POPOVER
 
   // ++++++++++++++++++ CODE THAT RETURNS ONE OF 3 POSSIBILITIES ++++++++++++++++++
   // 3 possibilities: still waiting for result, result found, or no results found
-  
+
   // STILL WAITING FOR RESULT: returns a spinner
   const spinner = (
     <div className={styles.spinner}>
@@ -74,13 +152,18 @@ export const SearchResult: React.FC = () => {
   // result function, which returns one of these 3 possibilities
   const result = () => {
     if (resultFound === undefined)
-    return spinner; // still waiting for data: return spinner
+      return spinner; // still waiting for data: return spinner
     else if (resultFound === false)
       return noResultsMessage; // no results found: return "no results found" message
     else if (resultFound === true) return productCards; // result found: return productCards
   };
 
-  return <div>{result()}</div>;
+  return (
+    <div>
+      {sortPopover()}
+      {result()}
+    </div>
+  );
 };
 
 export default SearchResult;
