a31a9f8e13861880ff05ec7fad68f730341beec9
KevinDutier desktop <kevin.dutier@gmail.com>
2023-01-25 18:42:29 +0100

    added captions to images

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import styles from "../styles/Banner.module.css";
+import Router from "next/router";
 
 export const Banner: React.FC = () => {
   return (
     <div>
-      <img src="images/banner0.png" className={styles.banner} />
+      <img
+        src="images/banner0.png"
+        className={styles.banner}
+        style={{ cursor: "pointer"}}
+        onClick={() =>
+          // redirects to asus laptop page on click
+          Router.push({
+            pathname: "./article",
+            query: { reference: "asus-tuf" },
+          })
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Banner;
--- components/Slideshow.tsx
+++ components/Slideshow.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../styles/Slideshow.module.css";
-import Router, { useRouter } from "next/router";
+import Router from "next/router";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
@@ -8,8 +8,8 @@ export const Slideshow: React.FC = () => {
   const clickBanner = (index: number) => {
     // redirects when clicking on an image
     if (index === 0) Router.push({ pathname: "./article", query: {reference: "lenovo-thinkpad-x1"}}) // 1st image, redirects to thinkpad x1
-    if (index === 1) Router.push({ pathname: "./article", query: {reference: "lenovo-thinkpad-t14"}}) // 2nd image, redirects to thinkpad t14
-    if (index === 2) Router.push({ pathname: "./article"}) // 3rd image, redirects to lenovo laptop
+    if (index === 1) Router.push({ pathname: "./search", query: {parameter: "lenovo"}}) // 2nd image, redirects to lenovo
+    if (index === 2) Router.push({ pathname: "./search", query: {parameter: "samsung"}}) // 3rd image, redirects to samsung
   }
 
   return (
